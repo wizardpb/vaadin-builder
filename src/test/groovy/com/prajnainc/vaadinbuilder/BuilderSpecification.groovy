8cbcf2d8fa55a5267e38bfc04fae7e92c8b26ceb
Paul Bennett <paul@prajnainc.com>
2014-04-16 00:39:40 -0400

    Added Menu building Refactored container addition

---
+++ src/test/groovy/com/prajnainc/vaadinbuilder/BuilderSpecification.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014 Prajna Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.prajnainc.vaadinbuilder;
+/**
+ * Created by Paul Bennett on 4/15/14.
+ *
+ * BuilderSpecification
+ *
+ */
+
+import spock.lang.*
+
+abstract public class BuilderSpecification extends Specification {
+
+    def builder = new VaadinBuilder()
+
+}
